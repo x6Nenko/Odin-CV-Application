9d507f2dab90122a70a21df7b97294029b8b1f7c
Max I <clickkava@gmail.com>
2024-03-03 20:30:04 +0200

    Add feature to download CV as PDF

--- src/components/previewCV/Preview.jsx
+++ src/components/previewCV/Preview.jsx
@@ -3,47 +3,64 @@ import phoneIcon from "../../assets/phone-solid.svg"
 import emailIcon from "../../assets/envelope-solid.svg"
 import WorkExperienceItem from "./WorkExperienceItem"
 import EducationExperienceItem from "./EducationExperienceItem"
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 
 const Preview = ({ generalState, educationState, experienceState }) => {
+  const downloadPdfDocument = (rootElementId) => {
+    const input = document.getElementById(rootElementId);
+    html2canvas(input)
+      .then((canvas) => {
+          const imgData = canvas.toDataURL('image/png');
+          const pdf = new jsPDF();
+          pdf.addImage(imgData,'JPEG',2,2,canvas.width*0.2,canvas.height*0.2,"a","FAST");
+          pdf.save("fastcv.pdf");
+      })
+  }
+
   return (
-    <section className="previewSection">
-      <article>
-        <h1 className="name">{generalState.firstName} {generalState.lastName}</h1>
-      </article>
+    <>
+      <button className="downloadBtn" onClick={() => downloadPdfDocument("previewSection")}>Download PDF</button>
 
-      <article className="contacts">
-        <div className="contact-holder">
-            <img src={emailIcon} alt="" className="contactIcon" aria-hidden="true" />
-            {generalState.email}
-        </div>
+      <section className="previewSection" id="previewSection">
+        <article>
+          <h1 className="name">{generalState.firstName} {generalState.lastName}</h1>
+        </article>
 
-        <div className="contact-holder">
-            <img src={phoneIcon} alt="" className="contactIcon" aria-hidden="true" />
-            {generalState.phone}
-        </div>
-      </article>
+        <article className="contacts">
+          <div className="contact-holder">
+              <img src={emailIcon} alt="" className="contactIcon" aria-hidden="true" />
+              {generalState.email}
+          </div>
 
-      <article className="workExperience">
-          <h2 className="experienceHeader">Work Experience</h2>
-          {experienceState.map((entry, index) => (
-            <WorkExperienceItem 
+          <div className="contact-holder">
+              <img src={phoneIcon} alt="" className="contactIcon" aria-hidden="true" />
+              {generalState.phone}
+          </div>
+        </article>
+
+        <article className="workExperience">
+            <h2 className="experienceHeader">Work Experience</h2>
+            {experienceState.map((entry, index) => (
+              <WorkExperienceItem 
+                key={index}
+                entry={entry}
+              />
+            ))}
+        </article>
+
+        <article className="educationExperience">
+          <h2 className="experienceHeader">Education</h2>
+          {educationState.map((entry, index) => (
+            <EducationExperienceItem 
               key={index}
               entry={entry}
             />
           ))}
-      </article>
-
-      <article className="educationExperience">
-        <h2 className="experienceHeader">Education</h2>
-        {educationState.map((entry, index) => (
-          <EducationExperienceItem 
-            key={index}
-            entry={entry}
-          />
-        ))}
-      </article>
-    
-    </section>
+        </article>
+      
+      </section>
+    </>
   )
 }
 
